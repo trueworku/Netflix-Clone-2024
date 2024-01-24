1ac0952dce666df76934d14a9a5f47cfad67c436
true <trueworku@gmail.com>
2024-01-24 02:54:44 -0500

    row

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
 import Banner from '../../Components/Banner/Banner';
+import RowList from '../../Components/Rows/RowList/RowList';
 
 
 
@@ -10,6 +11,7 @@ const Home = () => {
         <div>
            <Header/>
            <Banner/>
+           <RowList/>
             <Footer/>
            
         </div>
